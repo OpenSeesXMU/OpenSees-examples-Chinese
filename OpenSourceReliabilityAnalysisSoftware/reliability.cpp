3b4f70e75626d5f89d25dbcf745b5cf1fa6c94a8
LeiWang <41924367+Lew007@users.noreply.github.com>
2020-12-27 16:35:04 +0800

    move ORA to another repository

--- OpenSourceReliabilityAnalysisSoftware/reliability.cpp
+++
@@ -1,9 +0,0 @@
-#include "tcl.h"
-int reliability(ClientData clientData, Tcl_Interp *interp, int argc, char *argv[]) {
-	if (1) {
-		//theReliabilityBuilder = new TclReliabilityBuilder(theDomain, interp);
-		return TCL_OK;
-	}
-	else
-		return TCL_ERROR;
-}
